d13dc3116702cbe30192a3c36d5b11e9e72062d2
David Bonnecaze <davidbonnecaze@Davids-MacBook-Pro.local>
2020-07-03 10:25:33 -0400

    End of Section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -75,10 +75,7 @@ namespace DatingApp.API.Controllers
 
             return Ok(new {
                 token = tokenHandler.WriteToken(token)
-            });           
+            });
         }
-
-        
-
     }
 }
